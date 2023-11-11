2bc067a5b5c1056c969424931a6322ba90dff6ab
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2023-11-11 15:43:23 +0300

    fix

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+import gulp from 'gulp';
+import {path} from './gulp/config/path.js';
+import {plugins} from './gulp/config/plagins.js'
+
+import {copy} from './gulp/tasks/copy.js';
+import {reset} from './gulp/tasks/reset.js';
+import {html} from './gulp/tasks/html.js';
+import {server} from './gulp/tasks/server.js';
+import {scss} from './gulp/tasks/scss.js'; 
+import {js} from './gulp/tasks/js.js';
+import {images} from './gulp/tasks/images.js' 
+
+
+import ghPages from 'gulp-gh-pages';
+
+// const deploy = () => {
+//     return app.gulp.src('./dist/**/*')
+//     .pipe(ghPages());
+// }
+
+// const publish = () => {
+//     return app.gulp.src('./dist/**/*')
+//     .pipe(ghPages(
+//         { 
+//             remoteUrl: "https://veronadracula.github.io/test/",
+//             branch: "gh-pages"
+//         }
+//     ));
+// }
+
+// gulp.task('publish', function () {
+//     return gulp.src("./dist/**/*")
+//       .pipe(ghPages())
+//   });
+
+gulp.task('deploy', function() {
+    return gulp.src(app.path.buildFolder)
+      .pipe(ghPages(
+        
+      ));
+  });
+
+global.app = { 
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+function watcher () {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
+}
+
+
+const mainTasks = gulp.parallel(copy, html, scss, js, images)
+
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+// const deploy = gulp.series(build, publish);
+
+
+export {dev}
+export {build}
+// export {deploy}
+
+gulp.task('default', dev);
